84fce8553eb9fe893f7307480261236aab9bb95e
Aakash Pal <palakash27@gmail.com>
2022-10-13 01:48:19 -0400

    Add homepage and sprint-1 docs

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,75 @@
-import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 import styles from "../styles/Home.module.css";
 import withHeaderAndFooter from "../hooks/withHeaderAndFooter";
 
 function Home() {
     return (
         <div>
-            <header>
-                <nav>
-                    <div className="logo">
-                        <h3>PAW CLINIC</h3>
-
-                        <img
-                            src="/images/mlogo.png"
-                            alt="logo"
-                            height={60}
-                            width={60}
-                        />
-                    </div>
-                    <ul>
-                        <li>
-                            <Link href="">HOME</Link>
-                        </li>
-                        <li>
-                            <Link href="/login">LOGIN</Link>
-                        </li>
-                        <li>
-                            <Link href="/signup">SIGNUP</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <hr className="solid" />
-            <div className="container">
-                <img
-                    className="container_image"
-                    src="images/mlogo.png"
-                    alt="welcome"
+            <div>
+                <Image
+                    src="/images/header.png"
+                    alt="image"
+                    width="1800px"
+                    height="700px"
                 />
-                <div className="content">
-                    <h2>Lorem Ispum</h2>
+            </div>
+            <div className={styles.container}>
+                <Image
+                    src="/images/mlogo.png"
+                    alt="welcome"
+                    width="80px"
+                    height="70px"
+                />
+                <div className={styles.content}>
+                    <h2>DOGS</h2>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry&apos;s standard dummy text ever since the
-                        1500s, when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                    <h2>Lorem Ispum</h2>
-                    <p>
-                        1. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        No matter what kind or number of pets you have. This
+                        application is going to make your life a piece of cake
+                        with maintaining records.
                     </p>
                     <p>
-                        2. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.{" "}
+                        2. This website is under development version and
+                        requires a user to log in to view the pet-related
+                        dashboard and the number of pets any user has.{" "}
                     </p>
                     <p>
-                        3. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        3. A dog’s sense of smell is so precise, it could detect
+                        a teaspoon of sugar in a million gallons of water, or
+                        two Olympic-sized pools worth. A dog’s nose print is
+                        unique, much like a person’s fingerprint. When dogs kick
+                        backward after they go to the bathroom it’s not to cover
+                        it up, but to mark their territory, using the scent
+                        glands in their feet.
                     </p>
-                    <p>
-                        4. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                    <p>
-                        5. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                    <p>
-                        6. It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
+                    <div>
+                        4. Interesting cat facts:
+                        <ul>
+                            <li>
+                                A cat has 32 muscles in each ear, humans only
+                                have 6.
+                            </li>
+                            <li>Cats spend 15-20% of their time grooming.</li>
+                            <li>Cats have more than 100 vocal sounds.</li>
+                            <li>
+                                A young, healthy, average-sized cat can jump
+                                about six times their length or over eight feet
+                                in a single bound.{" "}
+                            </li>
+                            <li>
+                                Cats have a sensory organ at the end of their
+                                whiskers called a proprioceptor, which sends
+                                tactile signals to the brain and nervous system.
+                            </li>
+                            <li>
+                                Cats are able to rotate their supple spines more
+                                than many other animals and can twist their
+                                bodies to a much greater extent.
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <hr className="solid" />
         </div>
     );
 }
